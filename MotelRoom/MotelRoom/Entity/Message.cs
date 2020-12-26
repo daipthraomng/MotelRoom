4672cf9271511401c0f3dba9b4ce9cf5eda86cbe
daipthraomng <52918894+daipthraomng@users.noreply.github.com>
2020-12-26 19:56:21 +0700

    #10 class message, thêm để sửa lỗi

---
+++ MotelRoom/MotelRoom/Entity/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotelRoom.Entity
+{
+    public class Message
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int idMessage { get; set; }
+        public string name { get; set; }
+        public string contentMessage { get; set; }
+    }
+}
