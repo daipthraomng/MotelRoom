764679d42bd8db644534718a47d8b37057cad148
daipthraomng <52918894+daipthraomng@users.noreply.github.com>
2020-12-27 21:45:43 +0700

    #12 Update model to admin screen post, roominfo

--- MotelRoom/MotelRoom/Models/RoomInfoModel.cs
+++ MotelRoom/MotelRoom/Models/RoomInfoModel.cs
@@ -15,10 +15,12 @@ namespace MotelRoom.Models
         public List<ImageRoom> listImageRoom { get; set; }
         public List<RoomSummaryInfo> listRoomSummary { get; set; }
         public AddressModel address { get; set; }
+        public Owner owner { get; set; }
         public RoomInfoModel() { 
             listRoom = new List<Room>();
             address = new AddressModel();
             listRoomSummary = new List<RoomSummaryInfo>();
+            owner = new Owner();
         }
         public void GetListRoomSummaryInfo()
         {
@@ -59,45 +61,45 @@ namespace MotelRoom.Models
                 }
             }
         }
-        public void GetRoomInfo(int id)
-        {
-            var connect = new DataProvider().GetSqlConnectionProvider(); // get connect
+        //public void GetRoomInfo(int id)
+        //{
+        //    var connect = new DataProvider().GetSqlConnectionProvider(); // get connect
 
-            using (var objConnect = connect)
-            {
-                var objCmd = new SqlCommand
-                {
-                    Connection = objConnect,
-                    CommandText = "sp_GetRoomInfo", // name stored procedure
-                    CommandType = CommandType.StoredProcedure
-                };
+        //    using (var objConnect = connect)
+        //    {
+        //        var objCmd = new SqlCommand
+        //        {
+        //            Connection = objConnect,
+        //            CommandText = "sp_GetRoomInfo", // name stored procedure
+        //            CommandType = CommandType.StoredProcedure
+        //        };
 
-                try
-                {
-                    objCmd.Parameters.AddWithValue("@id", SqlDbType.Int).Value = id;
-                    // Open connection
-                    objConnect.Open();
-                    //Get data from database
-                    DataSet ds = new DataSet();
-                    SqlDataAdapter da = new SqlDataAdapter();
-                    da.SelectCommand = objCmd;
-                    da.Fill(ds); // fill dataset by adapter
-                    if (ds.Tables.Count > 0)
-                    {
-                        this.listRoom = BaseModel.ConvertDataTable<Room>(ds.Tables[0]);
-                    }
-                }
-                catch (Exception ex)
-                {
+        //        try
+        //        {
+        //            objCmd.Parameters.AddWithValue("@id", SqlDbType.Int).Value = id;
+        //            // Open connection
+        //            objConnect.Open();
+        //            //Get data from database
+        //            DataSet ds = new DataSet();
+        //            SqlDataAdapter da = new SqlDataAdapter();
+        //            da.SelectCommand = objCmd;
+        //            da.Fill(ds); // fill dataset by adapter
+        //            if (ds.Tables.Count > 0)
+        //            {
+        //                this.listRoom = BaseModel.ConvertDataTable<Room>(ds.Tables[0]);
+        //            }
+        //        }
+        //        catch (Exception ex)
+        //        {
 
 
-                }
-                finally
-                {
-                    objConnect.Close();
-                }
-            }
-        }
+        //        }
+        //        finally
+        //        {
+        //            objConnect.Close();
+        //        }
+        //    }
+        //}
 
     }
 }
